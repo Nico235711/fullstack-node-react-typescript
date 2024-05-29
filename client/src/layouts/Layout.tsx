ce01235432a821593be48a9c7bce6d0d7ffcaa07
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-29 12:15:22 -0300

    Trabajando con el Layout

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -4,10 +4,16 @@ const Layout = () => {
 
   return (
     <>
-      <div>Desde Layout</div>
+      <header className="bg-slate-800">
+        <div className="mx-auto max-w-6xl py-5">
+          <h1 className="font-extrabold text-4xl text-white">Administrador de Productos</h1>
+        </div>
+      </header>
 
       {/* inyecta el contenido manteniendo el div */}
-      <Outlet />
+      <main className="mt-10 mx-auto max-w-6xl bg-white shadow p-10">
+        <Outlet />
+      </main>
     </>
   )
 }
