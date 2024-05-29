48480f052fd85be3d4feeb6941205a7176595773
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-29 15:31:39 -0300

    Introducción a los Loaders en React Router

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,6 +1,16 @@
-import { number, object, string } from "valibot";
+import { boolean, number, object, string, InferOutput, array } from "valibot";
 
 export const DraftProductSchema = object({
   name: string(),
   price: number()
 })
+
+const ProductSchema = object({
+  id: number(),
+  name: string(),
+  price: number(),
+  availability: boolean()
+})
+export const ProductsSchema = array(ProductSchema)
+
+export type Product = InferOutput<typeof ProductSchema>
