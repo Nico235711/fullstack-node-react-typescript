7323fe76a83ed41e5b41ef2e11786334b1819042
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-29 13:35:49 -0300

    Validando los datos con ZOD antes de enviarlos a la REST API

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,9 +1,23 @@
+import { safeParse } from "valibot";
+import { DraftProductSchema } from "../types";
 
 type addProductProps = {
   [k: string]: FormDataEntryValue
 }
 
 export async function addProduct(data: addProductProps) {
-  console.log(data);
-  
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price
+    })
+    
+    if (result.success) return result
+    else throw new Error("Datos no válidos");
+    
+
+  } catch (error) {
+    console.log(error);
+    
+  }
 }
