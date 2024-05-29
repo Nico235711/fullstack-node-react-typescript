140635e986e5af525b6df32ae1d8f62406cc04f4
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-29 13:47:05 -0300

    Enviando la petición hacia la REST API

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,5 +1,6 @@
 import { safeParse } from "valibot";
 import { DraftProductSchema } from "../types";
+import axios from "axios";
 
 type addProductProps = {
   [k: string]: FormDataEntryValue
@@ -12,7 +13,15 @@ export async function addProduct(data: addProductProps) {
       price: +data.price
     })
     
-    if (result.success) return result
+    if (result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products`
+      const { data: dataProduct } = await axios.post(url, {
+        name: result.output.name,
+        price: result.output.price
+      })
+      console.log(dataProduct);
+      
+    }
     else throw new Error("Datos no válidos");
     
 
