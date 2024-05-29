cf006448b199340a739d5eb3de54f4482e49125f
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-29 14:19:58 -0300

    Habilitando CORS en el Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import router from './router';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './config/swagger';
+import cors, { CorsOptions } from 'cors';
 
 // Conectar con la DB
 async function connectDB() {
@@ -23,6 +24,20 @@ connectDB()
 
 // instancia de express
 const server = express()
+
+// permitir conexiones
+const corsOptions: CorsOptions = {
+  origin: function(origin, callback) {
+    // console.log(origin);
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true)
+    }
+    else {
+      callback(new Error("Error de CORS"))
+    }
+  }
+}
+server.use(cors(corsOptions))
 
 // leer datos de formularios
 server.use(express.json())
