060e0716662d7808917a882dcbe9c2f9fe11f31f
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-29 12:23:35 -0300

    Routing entre las diferentes páginas

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom"
 
 const NewProduct = () => {
 
   return (
-    <div>NewProduct</div>
+    <>
+      <div className="flex justify-between items-center">
+        <h2 className="text-4xl font-extrabold text-slate-500">Registrar Producto</h2>
+        <Link
+          to="/"
+          className="bg-blue-800 py-1 px-2 text-lg text-white font-bold rounded-md hover:bg-blue-900 transition-all"
+        >Volver</Link>
+      </div>
+    </>
   )
 }
 
--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom"
 
 const Products = () => {
 
   return (
-    <div>Products</div>
+    <>
+      <div className="flex justify-between items-center">
+        <h2 className="text-4xl font-extrabold text-slate-500">Productos</h2>
+        <Link
+          to="/productos/nuevo"
+          className="bg-blue-800 py-1 px-2 text-lg text-white font-bold rounded-md hover:bg-blue-900 transition-all"
+        >Agregar Producto</Link>
+      </div>
+    </>
   )
 }
 
