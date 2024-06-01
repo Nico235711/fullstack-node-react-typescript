ec2a722d6bceb9989bf9d72b099ce5b053c33a6f
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-06-01 20:41:51 -0300

    Creando la función para eliminar Productos

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,9 +1,17 @@
-import { useNavigate } from "react-router-dom"
+import { ActionFunctionArgs, Form, redirect, useNavigate } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
+import { deleteProduct } from "../services/ProductService"
 
 type ProductDetailsProps = {
   product: Product
+}
+
+export async function action({ params }: ActionFunctionArgs ) {
+  if (params.id !== undefined) {
+    await deleteProduct(+params.id)
+    return redirect("/")
+  }
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
@@ -29,9 +37,20 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
             className="bg-indigo-600 py-1 px-2 text-lg text-white font-bold rounded-md hover:bg-blue-700 transition-all"
             onClick={() => navigate(`productos/${product.id}/editar`)}
           >Editar</button>
-          <button
-            type="button"
-            className="bg-red-600 py-1 px-2 text-lg text-white font-bold rounded-md hover:bg-red-700 transition-all">Eliminar</button>
+
+          <Form
+            method="post"
+            action={`productos/${product.id}/eliminar`}
+            onSubmit={e => {
+              if (!confirm("Desea eliminar?"))
+                e.preventDefault()
+            }}
+          >
+            <button
+              type="submit"
+              className="bg-red-600 py-1 px-2 text-lg text-white font-bold rounded-md hover:bg-red-700 transition-all"
+            >Eliminar</button>
+          </Form>
         </div>
       </td>
     </tr>
--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -13,49 +13,49 @@ export async function addProduct(data: ProductData) {
       name: data.name,
       price: +data.price
     })
-    
+
     if (result.success) {
       const url = `${import.meta.env.VITE_API_URL}/api/products`
       await axios.post(url, {
         name: result.output.name,
         price: result.output.price
-      })      
+      })
     }
     else throw new Error("Datos no válidos");
-    
+
 
   } catch (error) {
     console.log(error);
-    
+
   }
 }
 
 export async function getProducts() {
   try {
-      const url = `${import.meta.env.VITE_API_URL}/api/products`
-      const { data } = await axios(url)
-      const result = safeParse(ProductsSchema, data.data)
-      if (result.success) return result.output
-      else throw new Error("Hubo un error...");
+    const url = `${import.meta.env.VITE_API_URL}/api/products`
+    const { data } = await axios(url)
+    const result = safeParse(ProductsSchema, data.data)
+    if (result.success) return result.output
+    else throw new Error("Hubo un error...");
 
   } catch (error) {
     console.log(error);
-    
+
   }
 }
 
 export async function getProductById(id: Product["id"]) {
   try {
-      const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
-      const { data } = await axios(url)
-      const result = safeParse(ProductSchema, data.data)
-      
-      if (result.success) return result.output
-      else throw new Error("Hubo un error...");
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+    const { data } = await axios(url)
+    const result = safeParse(ProductSchema, data.data)
+
+    if (result.success) return result.output
+    else throw new Error("Hubo un error...");
 
   } catch (error) {
     console.log(error);
-    
+
   }
 }
 
@@ -72,7 +72,7 @@ export async function updateProduct(data: ProductData, id: Product["id"]) {
       availability: toBoolean(data.availability.toString())
     })
     // console.log(result);
-    
+
     if (result.success) {
       const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
       await axios.put(url, result.output)
@@ -80,6 +80,17 @@ export async function updateProduct(data: ProductData, id: Product["id"]) {
 
   } catch (error) {
     console.log(error);
-    
+
+  }
+}
+
+export async function deleteProduct(id: Product["id"]) {
+
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+    await axios.delete(url)
+
+  } catch (error) {
+    console.log(error);
   }
 }
