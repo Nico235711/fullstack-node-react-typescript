eadfb6946f300dc818bdd484a1375488d748d00e
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-06-01 18:55:37 -0300

    Agregando la disponibilidad en edición

--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -1,6 +1,6 @@
 import { ActionFunctionArgs, Form, Link, LoaderFunctionArgs, redirect, useActionData, useLoaderData } from "react-router-dom"
 import Error from "../components/Error"
-import { addProduct, getProductById, updateProduct } from "../services/ProductService"
+import { getProductById, updateProduct } from "../services/ProductService"
 import { Product } from "../types"
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -9,7 +9,7 @@ export async function loader({ params }: LoaderFunctionArgs) {
     if (!product) return redirect("/")
     return product
   }
-  
+
 }
 
 export async function action({ request, params }: ActionFunctionArgs) {
@@ -29,6 +29,11 @@ export async function action({ request, params }: ActionFunctionArgs) {
   }
   return redirect("/")
 }
+
+const availabilityOptions = [
+  { name: 'Disponible', value: true },
+  { name: 'No Disponible', value: false }
+]
 
 const EditProduct = () => {
 
@@ -65,6 +70,7 @@ const EditProduct = () => {
             defaultValue={product.name}
           />
         </div>
+
         <div className="mb-4">
           <label
             className="text-gray-800"
@@ -79,6 +85,24 @@ const EditProduct = () => {
             defaultValue={product.price}
           />
         </div>
+        
+        <div className="mb-4">
+          <label
+            className="text-gray-800"
+            htmlFor="availability"
+          >Disponibilidad:</label>
+          <select
+            id="availability"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            name="availability"
+            defaultValue={product?.availability.toString()}
+          >
+            {availabilityOptions.map(option => (
+              <option key={option.name} value={option.value.toString()}>{option.name}</option>
+            ))}
+          </select>
+        </div>
+
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
