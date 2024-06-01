d493e184701094f091e4b62f4374215c242a030b
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-06-01 20:55:41 -0300

    mostrando los un párrafo si no hay productos

--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -22,27 +22,34 @@ const Products = () => {
           className="bg-indigo-800 py-1 px-2 text-lg text-white font-bold rounded-md hover:bg-indigo-900 transition-all"
         >Agregar Producto</Link>
       </div>
-
-      <div className="p-2">
-        <table className="w-full mt-5 table-auto">
-          <thead className="bg-slate-800 text-white">
-            <tr>
-              <th className="p-2">Producto</th>
-              <th className="p-2">Precio</th>
-              <th className="p-2">Disponibilidad</th>
-              <th className="p-2">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map(product => (
-              <ProductDetails 
-                key={product.id}
-                product={product}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {
+        products.length ? (
+          <>
+            <div className="p-2">
+              <table className="w-full mt-5 table-auto">
+                <thead className="bg-slate-800 text-white">
+                  <tr>
+                    <th className="p-2">Producto</th>
+                    <th className="p-2">Precio</th>
+                    <th className="p-2">Disponibilidad</th>
+                    <th className="p-2">Acci  ones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map(product => (
+                    <ProductDetails
+                      key={product.id}
+                      product={product}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </>
+        ) : (
+          <p className="mt-5 font-bold">No hay productos para mostrar, empiece a agregar productos y <span className="text-slate-500">aparecerán aquí</span></p>
+        )
+      }
     </>
   )
 }
