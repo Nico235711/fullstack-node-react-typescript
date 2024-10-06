9cc255a7b02c03ff58dd0217851546e4a425bb4e
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2024-10-06 20:02:56 -0300

    limpiando y empezando de cero

--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import server from "./server";
-import colors from 'colors'
-
-const port = process.env.DB_PORT || 4000
-
-server.listen(port, () => {
-  console.log(colors.cyan(`REST API en el puerto http://localhost:${port}`));
-})
