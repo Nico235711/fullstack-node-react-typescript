228b37347c3e4818c7469bdbf66c99ed62f0f418
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2024-10-07 12:24:02 -0300

    Conectando la Aplicación de Express a una base de datos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,21 @@
 import express from 'express'
 import productRouter from './routes'
+import db from './config/db'
 
+// conectar a DB
+async function connectDB() {
+  try {
+    await db.authenticate()
+    db.sync()
+    console.log("Conección exitosa a la BD");
+    
+  } catch (error) {
+    console.log(error);
+    console.log("Hubo un error al conectarse a la BD");
+    
+  }
+}
+connectDB()
 const server = express()
 server.use("/api/products", productRouter) // .use engloba todos los verbos http
 
