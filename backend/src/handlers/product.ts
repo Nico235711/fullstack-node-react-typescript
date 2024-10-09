4d0ef3eff7fb024437d0ba2d1e5148e9c21d7408
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2024-10-08 21:06:56 -0300

    finalizando los endpoints del CRUD

--- backend/src/handlers/product.ts
+++ backend/src/handlers/product.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
+const notFoundStatus = 404
+
 export const createProduct = async (req: Request, res: Response) => {
 
   try {
@@ -21,6 +23,67 @@ export const getAllProducts = async (req: Request, res: Response) => {
 
     const product = await Product.findAll();
     res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+
+  }
+};
+
+export const getProductById = async (req: Request, res: Response) => {
+
+  try {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+      res.status(notFoundStatus).json({ error: "Producto no encontrado" })
+      return
+    }
+    await product.destroy()
+    res.json({ data: product })
+
+  } catch (error) {
+    console.log(error);
+
+  }
+};
+
+export const updateProductById = async (req: Request, res: Response) => {
+
+  try {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+      res.status(notFoundStatus).json({ error: "Producto no encontrado" })
+      return
+    }
+    // actualizo
+    product.update(req.body)
+    res.json({ data: product })
+
+  } catch (error) {
+    console.log(error);
+
+  }
+};
+
+export const updateAvailability = async (req: Request, res: Response) => {
+
+  try {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+      res.status(notFoundStatus).json({ error: "Producto no encontrado" })
+      return
+    }
+    // actualizo
+    product.availability = !product.dataValues.availability
+    await product.save()
+    
+    res.json({ data: product })
+
   } catch (error) {
     console.log(error);
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express' // instacia del router para no tener que importar el server
-import { createProduct, getAllProducts } from './handlers/product'
-import { body } from 'express-validator'
+import { createProduct, getAllProducts, getProductById, updateAvailability, updateProductById } from './handlers/product'
+import { body, param } from 'express-validator'
 import { handleInputErrors } from './middleware'
 
 const router = Router()
@@ -20,5 +20,42 @@ router.post("/",
 )
 
 router.get("/", getAllProducts)
+
+router.get("/:id",
+  param("id")
+    .isInt().withMessage("Id no válido"),
+  handleInputErrors,
+  getProductById
+)
+
+router.put("/:id",
+  param("id")
+    .isInt().withMessage("Id no válido"),
+  body("name")
+  .notEmpty().withMessage("El nombre del producto no puede ir vacío"),
+  body("price")
+    .notEmpty().withMessage("El precio del producto no puede ir vacío")
+    .isNumeric().withMessage("Valor del producto no válido")
+    .custom(value => value > 0).withMessage("Precio del producto no válido"),
+  body("availability")
+    .notEmpty().withMessage("El valor de la disponibilidad no puede ir vacío")
+    .isBoolean().withMessage("Valor no válido"),
+  handleInputErrors,
+  updateProductById
+)
+
+router.patch("/:id",
+  param("id")
+    .isInt().withMessage("Id no válido"),
+  handleInputErrors,
+  updateAvailability
+)
+
+router.delete("/:id",
+  param("id")
+    .isInt().withMessage("Id no válido"),
+  handleInputErrors,
+  getProductById
+)
 
 export default router
