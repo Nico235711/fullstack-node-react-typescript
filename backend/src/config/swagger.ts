51b4a1d9b4550c8d246fb4fdc4cc0b62b8048de9
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2024-12-05 18:12:35 -0300

    terminando de configurar swagger

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -1,5 +1,22 @@
 import swaggerJSDoc from 'swagger-jsdoc'
 
 const options: swaggerJSDoc.Options = {
-  swaggerDefinition
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    tags: [
+      {
+        name: 'Productos',
+        description: 'Endpoints de productos'
+      }
+    ],
+    info: {
+      title: 'Documentación de la API de productos',
+      version: '1.0.0'
+    }
+  },
+  apis: ['src/routes/*.ts']
 }
+
+const swaggerSpecs = swaggerJSDoc(options)
+
+export default swaggerSpecs
